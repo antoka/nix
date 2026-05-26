255e9edc95f4ccc67036d3644f67d3a1739af6b6
Adam Antok <adam@antok.me>
2026-05-26 18:04:49 +0200

    media tools and devenv

--- common.nix
+++ common.nix
@@ -72,6 +72,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.trusted-users = [ "root" "antoka" ];
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
@@ -102,6 +103,9 @@
     ntfs3g
     exfatprogs
     mc
+    exiftool
+    ffmpeg
+    devenv
   ];
 
   fonts.packages = with pkgs; [
